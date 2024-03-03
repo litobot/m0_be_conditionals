f5a5d40d0375adfe2817684762ee0e08125deba9
Lito Croy <146680987+litobot@users.noreply.github.com>
2024-03-03 11:36:19 -0700

    Show mild.rb complete

---
+++ code_challenges/mild.rb
@@ -0,0 +1,13 @@
+# Write a Ruby program that defines a variable that stores an Integer. The program should print out the String "even" 
+# if the Integer is even, and the String "odd" if the Integer is odd.
+
+
+
+# define a variable that stores an integer
+integer_1 = 13
+
+if integer_1.odd?
+    puts "odd"
+else integer_1.even?
+    puts "even"
+end
