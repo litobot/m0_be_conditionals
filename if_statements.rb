fcd607fed18e79c8460876c0274019add83c94eb
Lito Croy <146680987+litobot@users.noreply.github.com>
2024-03-03 10:22:38 -0700

    Show if_statements.rb completed

--- if_statements.rb
+++ if_statements.rb
@@ -41,11 +41,12 @@ end
 # Experiment with manipulating the value held within num_quarters
 # to make sure both conditions can be achieved.
 
-num_quarters = 0
-
-puts "I have enough money for a gumball"
-puts "I don't have enough money for a gumball"
-
+num_quarters = 1
+if num_quarters >= 1
+  puts "I have enough money for a gumball"
+else num_quarters <= 0
+  puts "I don't have enough money for a gumball"
+end
 
 #####################
 # Using the variables defined below, write code that will tell you
@@ -61,5 +62,11 @@ puts "I don't have enough money for a gumball"
 # Experiment with manipulating the value held within both variables
 # to make sure all above conditions output what you expect.
 
-cups_of_flour = 1
+cups_of_flour = 3
 has_sauce = true
+
+if cups_of_flour >= 2 && has_sauce == true
+  puts "I can make pizza."
+else cups_of_flour < 2 || has_sauce == false
+  puts "I cannot make pizza."
+end
